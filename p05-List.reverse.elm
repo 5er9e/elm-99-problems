d5ad79de0060700bc35b32233f82b04c3d515cc6
5er9e <5erge@mail.com>
2018-02-28 21:33:41 -0600

    problem 05-List.reverse

---
+++ p05-List.reverse.elm
@@ -0,0 +1,37 @@
+-- Problem 5
+-- Elm provides the function List.reverse to reverse the order of elements in a list. See if you can implement it.
+-- Example
+-- myReverse [1..4] == [4, 3, 2, 1]
+-- Unit Test
+
+import Html exposing (text)
+import List 
+
+
+myReverse : List a -> List a
+myReverse xs =
+    -- your implemenation goes here
+    []
+
+
+main : Html.Html a      
+main =
+    Html.text 
+        <| case test of 
+            0 -> 
+                "Your implementation passed all tests."
+
+            n -> 
+                "Your implementation failed " ++ toString n ++ " tests." 
+
+
+test : Int
+test =
+    List.length
+        <| List.filter ((==) False)
+            [ myReverse [1, 2, 3, 4] == [4, 3, 2, 1] 
+            , myReverse [2, 1] == [1, 2] 
+            , myReverse [1] == [1] 
+            , myReverse [] == [] 
+            , myReverse [ 'a', 'b', 'c' ] == [ 'c', 'b', 'a' ]
+            ]
