9b897b594d29d2e62696a4495af4f48810740db6
5er9e <5erge@mail.com>
2018-03-04 21:23:38 -0600

    p07.e2-List.takeWhile

---
+++ p07.e2-List.takeWhile.elm
@@ -0,0 +1,44 @@
+-- Extra #2 - takeWhile
+-- Keep elements from the start of a list while they satisfy a condition.
+-- Example
+-- takeWhile isEven [2, 4, 6, 7, 8] == [2, 4, 6]
+-- Unit Test
+import Html exposing (text)
+import List
+
+
+takeWhile : (a -> Bool) -> List a -> List a
+takeWhile predicate list =
+    -- your implementation here
+    []
+
+
+main : Html.Html a
+main =
+    Html.text
+        <| case test of
+            0 ->
+                "Your implementation passed all tests."
+
+            x ->
+                "Your implementation failed " ++ (toString x) ++ " tests."
+
+
+test : Int
+test =
+    List.length
+        <| List.filter ((==) False)
+          [ (takeWhile isOdd [ 1, 2, 1 ] == [ 1 ])
+          , (takeWhile isEven [ 1, 2, 1 ] == [])
+          , (takeWhile isEven [] == [])
+          , (takeWhile isEven [ 2, 4, 100000, 1 ] == [ 2, 4, 100000 ])
+          , (takeWhile ((>) 5) (List.range 1 10) == [ 1, 2, 3, 4 ])
+          , (takeWhile ((>) 50) (List.range 1 10) == List.range 1 10)
+
+
+isEven x =
+    x % 2 == 0
+
+
+isOdd x =
+    x % 2 /= 0
